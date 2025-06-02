328c3fc3f5f53dd3c0584e9089fe2c9e91deb1f6
mbalderas08@alumnos.uaq.mx <mbalderas08@alumnos.uaq.mx>
2025-06-02 16:30:11 -0600

    fix: change the cookies name

--- frontend/middleware.ts
+++ frontend/middleware.ts
@@ -2,7 +2,7 @@ import { NextRequest, NextResponse } from "next/server";
 
 export default function MiddleWare(req: NextRequest) {
   if (req.nextUrl.pathname.startsWith('/dashboard')){
-    const token = req.cookies.get("cinerex-")?.value;
+    const token = req.cookies.get("user_for_cinerex")?.value;
     if (!token) {
       return NextResponse.redirect(new URL('/login', req.url))
     }
